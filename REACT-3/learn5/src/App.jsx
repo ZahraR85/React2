ffbb24f803528136422ddbfa83add37c5d9ca745
Shabnam <zahra.reihani85@gmail.com>
2024-10-20 21:34:59 +0200

    change of learn5

--- REACT-3/learn5/src/App.jsx
+++ REACT-3/learn5/src/App.jsx
@@ -9,12 +9,22 @@ const Form = () => {
   });
 
   const handleChange = (e) => {
-    setForm({
-      ...form,
-      [e.target.name]: e.target.value,
+    setForm({...form,[e.target.name]: e.target.value,
     });
   };
-
+const handleSubmit = (e)=> {
+  e.preventDefault();
+  if (! form.firstName || ! form.lastName || !form.email || ! form.message){
+    alert("All fields required!");
+  }
+    console.log(form);
+   /* setForm = ({
+      firstName: '',
+      lastName: '',
+      email: '',
+      message: '',
+  }); */
+  };
   return (
     <>
       <label>
@@ -40,6 +50,7 @@ const Form = () => {
       <p>
         My Name is {form.firstName} {form.lastName} and my Email address is: {form.email} . I have this message:{form.message}
       </p>
+      <button style={{padding: "20px"}} onClick={handleSubmit}>Submit</button>
     </>
   );
 };
